fad73f8f1c43f6bb4d864ba1cb4ba2b4bfe4c43c
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-12-05 14:02:01 +0100

    Api settings

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './scss/style.scss';
+import './js/polyfills';
 import './js/app';
--- src/js/notes-actions.js
+++ src/js/notes-actions.js
@@ -1,26 +1,11 @@
-let notesList = [
-   {
-      id: 1,
-      title: 'notatka 1',
-      body: '# asdfg',
-      lastModified: 1544010561795
-   },
-   {
-      id: 2,
-      title: 'notatka 2',
-      lastModified: 1544010561795
-   },
-   {
-      id: 3,
-      title: 'notatka 3',
-      lastModified: 1544010561795
-   },
-   {
-      id: 4,
-      title: 'notatka 4',
-      lastModified: 1544010561795
-   }
-];
+import axios from './axios-api';
+
+axios.get('/notes').then(function(response) {
+   // handle success
+   console.log(response);
+});
+
+let notesList = [];
 
 export function getAll() {
    return notesList;
